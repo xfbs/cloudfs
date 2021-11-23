c4e7540018861b2bb0686b2456fccd85593d143b
Patrick Elsen <pelsen@xfbs.net>
2021-11-24 00:06:01 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# CloudFS
+
+Cloud-native filesystem. 
--- src/storage.rs
+++ src/storage.rs
@@ -1,16 +1,41 @@
 use bytes::Bytes;
-//use anyhow::Result;
 use crate::hash::Hash;
-use std::error::Error;
+use std::error::Error as StdError;
+use thiserror::Error;
 
-#[async_trait::async_trait]
-trait StorageRead<E: Error> {
-    fn chunk_get(&self, hash: &Hash) -> Result<Option<Bytes>, E>;
-    fn block_get(&self, hash: &Hash) -> Result<Option<Bytes>, E>;
-    fn entry_get(&self, hash: &Hash) -> Result<Option<Bytes>, E>;
-    fn tree_get(&self, hash: &Hash) -> Result<Option<Bytes>, E>;
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum HashAlgorithm {
+    Sha512,
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Context {
+    hash_algorithm: HashAlgorithm,
+    chunk_size: usize,
+    block_size: usize,
+    entry_size: usize,
+    tree_size: usize
+}
+
+#[derive(Error, Clone, Debug)]
+pub enum StorageReadError<E: StdError> {
+    #[error("Got data with an invalid length")]
+    InvalidLength,
+    #[error("Got data with an invalid hash")]
+    InvalidHash,
+    #[error("Other error has occured: {0:}")]
+    Other(E),
 }
 
 #[async_trait::async_trait]
-trait StorageWrite {
+pub trait StorageRead<E: StdError> {
+    async fn chunk_get(&self, context: &Context, hash: &Hash) -> Result<Option<Bytes>, StorageReadError<E>>;
+    async fn block_get(&self, context: &Context, hash: &Hash) -> Result<Option<Bytes>, StorageReadError<E>>;
+    async fn entry_get(&self, context: &Context, hash: &Hash) -> Result<Option<Bytes>, StorageReadError<E>>;
+    async fn tree_get(&self, context: &Context, hash: &Hash) -> Result<Option<Bytes>, StorageReadError<E>>;
+    async fn head_get(&self, context: &Context, hash: &Hash) -> Result<Option<Bytes>, StorageReadError<E>>;
+}
+
+#[async_trait::async_trait]
+trait StorageWrite<E: StdError> {
 }
